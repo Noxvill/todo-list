0472c9511d946313d5b079b7141946ac16eb3942
Noxvill <gmo_mmo@live.cl>
2023-12-17 22:00:22 -0300

    Cambios en lógicas

---
+++ assets/js/index.js
@@ -0,0 +1,141 @@
+// capturando elementos del dom
+
+const spantotal = document.querySelector(".spantotal")
+const spanrealizadas = document.querySelector(".spanrealizadas")
+const ultask = document.querySelector(".ultask")
+// const totaltaskdone = document.querySelector(".totaltaskdone")
+const tarea = document.querySelector(".tarea")
+const btnadd = document.querySelector(".btnadd")
+const chkbx = document.querySelector(".chkbx")
+
+
+// Función para crear número aleatorio
+
+function randomnumberint(min, max) {
+    min = Math.ceil(1);
+    max = Math.floor(100);
+    return Math.floor(Math.random() * (100 - 1) + 1);
+  }
+
+// Definiendo Array
+
+let tareas = [
+
+{
+
+    id:"01",
+    name:"caminar",
+    done:false
+
+},
+
+{
+
+    id:"02",
+    name:"correr",
+    done:true
+
+},
+{
+
+    id:"03",
+    name:"Dormir",
+    done:false
+
+},
+
+]
+
+// Función para renderizar el Li con los datos del objeto
+
+const renderlitask = (id, name, done) => (
+
+`<li>
+<pan>${id}</pan>,
+<pan>${name}</pan>,
+<pan>${done}</pan>,
+
+<input id="chkbx" type="checkbox" onclick="markasdonetask('${id}')">${done ? "Marcar como no realizada":"Marcar como Realizada" }</input>
+<button type="button" class="btn-close text-danger" aria-label="Close" onclick="deletetask('${name}')"></button>
+
+</li>`
+)
+
+// Función para agregar tarea al arreglo
+
+const addtask = () => {
+
+if (tarea.value != ""){
+     tareas.push(
+{
+    id: randomnumberint(),
+    name: tarea.value,
+    done: false
+}
+  )
+
+  alert("La tarea: " + "'" + tarea.value + "'" + " fue agregada con éxito")
+  tarea.value=''
+  rendertask()
+
+} else{
+
+alert("El campo está vacío")
+
+}}
+
+// Agregando addEventListener al botón "Agregar" del formulario
+btnadd.addEventListener('click', addtask)
+
+// Función para eliminar tarea
+const deletetask =(nametaskdelete)=>{
+
+tareas = tareas.filter((tarea) => tarea.name != nametaskdelete) 
+rendertask()
+
+}
+
+// Función para marcar una tarea como realizada
+const markasdonetask=(idtaskdone)=>{
+
+const tasktomark = tareas.filter((tarea)=> tarea.id == idtaskdone)
+const tasknotmarked = tareas.filter((tarea)=> tarea.id != idtaskdone)
+
+if(idtaskdone.length>0){
+
+tasktomark[0].done=!tasktomark[0].done
+tareas=[...tasktomark,...tasknotmarked]
+
+}
+
+rendertask()
+
+}
+
+// Función para renderizar tareas en form
+
+const rendertask = () =>{
+
+    // alert("estiy dentro de rendeertask")
+tareas = tareas.sort((a,b) => a.name.localeCompare(b.name));
+ultask.innerHTML=""
+for (mytask in tareas){
+
+ultask.innerHTML+=renderlitask(
+
+
+    tareas[mytask].id,
+    tareas[mytask].name,
+    tareas[mytask].done 
+    
+    // alert("estoy dentro de li")
+)
+
+}
+
+spantotal.innerHTML = tareas.length
+spanrealizadas.innerHTML= tareas.filter((mytask) => mytask.done==true).length
+
+};
+
+rendertask()
